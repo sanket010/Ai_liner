c09db39ca058cef715f77a96980bdf39f7a6f3d9
shubham79a <shubh79.nolan@gmail.com>
2025-02-13 21:37:11 +0530

    added vercel file for deployment

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -10,15 +10,15 @@ function Footer() {
 
             <div className='flex justify-between items-center pb-5'>
                 <div className='flex gap-3 items-center'>
-                    <img className='h-7 max-sm:hidden' src={assets.logo} alt="" />
+                    <img className='h-7 cursor-pointer max-sm:hidden' src={assets.logo} alt="" />
                     <img className='h-7 sm:hidden' src={assets.logo_icon} alt="" />
                     <p className='text-[#797484]'>|</p>
                     <p className='text-[#797484]'><span className='max-sm:hidden'>All right reserved. </span>Copyright @imagify</p>
                 </div>
                 <div className='flex gap-3 items-center'>
-                    <img className='rounded-full w-8 ' src={assets.facebook_icon} alt="" />
-                    <img className='rounded-full w-8' src={assets.twitter_icon} alt="" />
-                    <img className='rounded-full w-8' src={assets.instagram_icon} alt="" />
+                    <a href="https://www.facebook.com/profile.php?id=100041822179627"><img className='rounded-full w-8  cursor-pointer' src={assets.facebook_icon} alt="" /></a>
+                    <a href="https://x.com/Shubham22142019"><img className='rounded-full w-8 cursor-pointer' src={assets.twitter_icon} alt="" /></a>
+                    <a href="https://www.instagram.com/shubham_quant/"><img className='rounded-full w-8 cursor-pointer' src={assets.instagram_icon} alt="" /></a>
                 </div>
             </div>
         </div>
